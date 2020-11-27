c7d33653afb468519d7bec926bb761a6f1f8ed38
rezonmain <adiazgomez101@gmail.com>
2020-11-26 19:32:45 -0800

    fixed for mobile

--- js/main.js
+++ js/main.js
@@ -1,41 +1,27 @@
 const canvas = document.getElementById("game");
 const context = canvas.getContext("2d");
-
-// change grid according to canvas width and heigth so number of grid remains constant
-// this can only work if aspect ratio remains constant (it does)
-// const grid = 25; // Size is 500*500 so 40x40grid
-// How the function works, i need the grid to alwyas be 20x20(pixels?), so if y divide 650 by 26 i get 25,
-// so i divide the the current width to always get 25, later on, grid WILL divide the width, so
-// grid is just a divider number(25) because 650/25 = 26, and 500/25 = 20, so grid stays at 26x20
-let grid;
-const pixelSize = 25;
-function calcGridDivider() {
-  let w = canvas.getBoundingClientRect().width;
-  let h = canvas.getBoundingClientRect().height;
-
-  grid = w / pixelSize; // w numero de pixeles que quiero
-}
-
-window.onload = calcGridDivider();
-window.addEventListener("resize", () => {
-  calcGridDivider();
-});
-
 const snakeStartxPos = 160;
 const snakeStartypos = 160;
-const SnakeStartSize = 4;
-const frameDivider = 4; // 60 / 4 = 15 fps
+const SnakeStartSize = 3;
+let frameDivider = 6; // 60 / 4 = 15 fps
 let score = 0;
 let gameState = false;
 let keyDownEvent;
 let killInput = false;
+let ranX;
+let ranY;
+
+let w = canvas.getBoundingClientRect().width;
+let h = canvas.getBoundingClientRect().height;
+const pixelSize = 16;
+let grid = Math.floor(w / pixelSize);
 
 // Get random int from 0 to max
 let getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
 };
 
-var snake = {
+let snake = {
   // Snake pos
   x: snakeStartxPos,
   y: snakeStartypos,
@@ -47,14 +33,52 @@ var snake = {
   maxCells: SnakeStartSize,
 };
 
-var burger = {
+ranX = getRandomInt(pixelSize);
+ranY = getRandomInt(pixelSize);
+let burger = {
   // TODO: start at random position not ocuppied by snake
-  x: getRandomInt(pixelSize) * grid,
-  y: getRandomInt(pixelSize) * grid,
+  x: ranX * grid,
+  y: ranY * grid,
   image: new Image(),
 };
-
 burger.image.src = "./img/b0.png";
+
+// change grid according to canvas width and heigth so number of grid remains constant
+// this can only work if aspect ratio remains constant (it does)
+// const grid = 25; // Size is 500*500 so 40x40grid
+// How the function works, i need the grid to alwyas be 20x20(pixels?), so if y divide 650 by 26 i get 25,
+// so i divide the the current width to always get 25, later on, grid WILL divide the width, so
+// grid is just a divider number(25) because 650/25 = 26, and 500/25 = 20, so grid stays at 26x20
+
+function calcGridDivider() {
+  w = canvas.getBoundingClientRect().width;
+  h = canvas.getBoundingClientRect().height;
+  canvas.setAttribute("height", h);
+  canvas.setAttribute("width", w);
+  grid = Math.floor(w / pixelSize); // w numero de pixeles que quiero
+  if (snake.dx !== 0) {
+    if (snake.dx < 0) {
+      snake.dx = -grid;
+    } else {
+      snake.dx = grid;
+    }
+  }
+  if (snake.dy !== 0) {
+    if (snake.dy < 0) {
+      snake.dy = -grid;
+    } else {
+      snake.dy = grid;
+    }
+  }
+  burger.x = ranX * grid;
+  burger.y = ranY * grid;
+  console.log(grid);
+}
+
+window.onload = calcGridDivider();
+window.addEventListener("resize", () => {
+  calcGridDivider();
+});
 
 function gameRestart() {
   gameState = 1;
@@ -65,42 +89,45 @@ function gameRestart() {
   burger.x = getRandomInt(pixelSize) * grid;
   burger.y = getRandomInt(pixelSize) * grid;
   score = 0;
+  frameDivider = 6;
   gameStart();
 }
-let blinkReq;
+let endReq;
+let blinkFrameCount = 0;
+let toggle = true;
 function gameEnd() {
   cancelAnimationFrame(req);
-  let toggle = true;
+  endReq = requestAnimationFrame(gameEnd);
+
+  let fontSize;
   // Display score and add overlay
-  //context.clearRect(0, 0, 500, 500); // Clear canvas
+  context.clearRect(0, 0, w, h); // Clear canvas
   context.shadowBlur = 10;
   context.shadowColor = "white";
   context.fillStyle = "white";
-  context.font = "40px 'Press Start 2P'";
-  context.fillText("Game over", 75, 200);
-  context.font = "10px 'Press Start 2P'";
-  context.fillText(`Obtuviste un puntaje de ${score} pts`, 100, 250);
+  fontSize = w / 12.5;
+  context.font = `${fontSize}px 'Press Start 2P'`;
+  context.fillText("Game over", w / 6.666, w / 2.5);
+  fontSize = w / 40;
+  context.font = `${fontSize}px 'Press Start 2P'`;
+  context.fillText(`Obtuviste un puntaje de ${score} pts`, w / 7, w / 2);
 
-  let blinkFrameCount = 0;
-
-  window.setTimeout(function blink() {
+  window.setTimeout(() => {
     killInput = false;
-    if (gameState === 1) {
-      return;
-    }
-    blinkReq = requestAnimationFrame(blink);
-    if (++blinkFrameCount < 30) {
-      return;
-    }
-    blinkFrameCount = 0;
+  }, 1000);
 
-    if (toggle) {
-      context.fillText("Oprime una tecla para continuar", 95, 300);
-    } else {
-      context.clearRect(0, 275, canvas.getBoundingClientRect().width, 40);
-    }
+  if (gameState === 1) {
+    cancelAnimationFrame(endReq);
+    return;
+  }
+  blinkFrameCount++;
+  if (blinkFrameCount >= 30) {
     toggle = !toggle;
-  }, 600);
+    blinkFrameCount = 0;
+  }
+  if (toggle) {
+    context.fillText("Oprime una tecla para continuar", w / 8, w / 1.666);
+  }
 }
 
 function gameStart() {
@@ -108,12 +135,11 @@ function gameStart() {
 }
 
 let req;
-
 let frameCount = 0;
 function gameloop() {
   req = requestAnimationFrame(gameloop); // Request a frame before it renders, call itself so it always updates
 
-  if (++frameCount < 4) {
+  if (++frameCount < frameDivider) {
     return; // Wait until 4 requestAnimations frames, to get 15fps, current frame is the fourth
   }
   frameCount = 0;
@@ -159,7 +185,7 @@ function gameloop() {
 // Prevent snake from eating itself, and ignore double preses
 document.addEventListener("keydown", (keyDownEvent) => {
   if (gameState === 0 && !killInput) {
-    cancelAnimationFrame(blinkReq);
+    cancelAnimationFrame(endReq);
     gameRestart();
   }
   if (
@@ -195,9 +221,42 @@ document.addEventListener("keydown", (keyDownEvent) => {
   }
 });
 
+function handleInput(input) {
+  document.getElementById("l").classList.remove("growAnimation");
+  document.getElementById("u").classList.remove("growAnimation");
+  document.getElementById("r").classList.remove("growAnimation");
+  document.getElementById("d").classList.remove("growAnimation");
+  if (gameState === 0 && !killInput) {
+    cancelAnimationFrame(endReq);
+    gameRestart();
+  }
+  if (input === "l" && snake.dx === 0) {
+    snake.dx = -grid;
+    snake.dy = 0;
+    document.getElementById(input).classList.add("growAnimation");
+  }
+  // up arrow key
+  else if (input === "u" && snake.dy === 0) {
+    snake.dy = -grid;
+    snake.dx = 0;
+    document.getElementById(input).classList.add("growAnimation");
+  }
+  // right arrow key
+  else if (input === "r" && snake.dx === 0) {
+    snake.dx = grid;
+    snake.dy = 0;
+    document.getElementById(input).classList.add("growAnimation");
+  }
+  // down arrow key
+  else if (input === "d" && snake.dy === 0) {
+    snake.dy = grid;
+    snake.dx = 0;
+    document.getElementById(input).classList.add("growAnimation");
+  }
+}
 function drawScreen() {
   context.fillStyle = "green";
-  context.shadowBlur = 10;
+  context.shadowBlur = 20;
   context.shadowColor = "green";
   snake.cells.forEach((cell, index) => {
     // drawing 1 px smaller than the grid creates a grid effect in the snake body so you can see how long it is
@@ -207,9 +266,19 @@ function drawScreen() {
     if (cell.x === burger.x && cell.y === burger.y) {
       snake.maxCells++;
       score++;
-      // canvas is 400x400 which is 25x25 grids
-      burger.x = getRandomInt(pixelSize) * grid;
-      burger.y = getRandomInt(pixelSize) * grid;
+      if (score > 15) {
+        frameDivider = 5;
+      }
+      if (score > 26) {
+        frameDivider = 4;
+      }
+      if (score > 50) {
+        frameDivider = 3;
+      }
+      ranX = getRandomInt(pixelSize);
+      ranY = getRandomInt(pixelSize);
+      burger.x = ranX * grid;
+      burger.y = ranY * grid;
     }
     for (let i = index + 1; i < snake.cells.length; i++) {
       // snake occupies same space as a body part. reset game
@@ -220,6 +289,7 @@ function drawScreen() {
       }
     }
   });
+
   // draw burger
   context.shadowBlur = 5;
   context.shadowColor = "#d6991e";
@@ -228,8 +298,9 @@ function drawScreen() {
   // draw score
   context.fillStyle = "white";
   context.shadowColor = "white";
-  context.font = "12px 'Press Start 2P'";
-  context.fillText(`SCORE ${score}`, 210, 30);
+
+  context.font = `${w / 38}px 'Press Start 2P'`;
+  context.fillText(`SCORE ${score}`, w / 2.3809, w / 16.666);
 }
 
 gameStart();
